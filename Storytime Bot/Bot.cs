833a77b4cc117b7bfc1d0c602f6a4ccb80c4e89c
LoganFlaherty <113549137+LoganFlaherty@users.noreply.github.com>
2024-10-23 18:24:29 -0400

    Add files via upload

---
+++ Storytime Bot/Bot.cs
@@ -0,0 +1,71 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.SlashCommands;
+using Storytime_Bot.Commands;
+
+namespace Storytime_Bot
+{
+    public class Bot
+    {
+        //fields
+        public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        //methods
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+            var config = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.AllUnprivileged.AddIntent(DiscordIntents.GuildMembers),
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            Client = new DiscordClient(config);
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(1)
+            });
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            var slashCommandsConfig = Client.UseSlashCommands();
+            slashCommandsConfig.RegisterCommands<RollCommands>(823715148715130891);
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
