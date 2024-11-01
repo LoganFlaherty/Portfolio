dd25d941d9fd9639e1d1bd8201d5093cad092d60
LoganFlaherty <113549137+LoganFlaherty@users.noreply.github.com>
2024-11-01 12:27:39 -0400

    Update Bot.cs

--- Storytime Discord Bot/Storytime Bot/Bot.cs
+++ Storytime Discord Bot/Storytime Bot/Bot.cs
@@ -57,7 +57,7 @@ namespace Storytime_Bot
             Commands = Client.UseCommandsNext(commandsConfig);
 
             var slashCommandsConfig = Client.UseSlashCommands();
-            slashCommandsConfig.RegisterCommands<RollCommands>(823715148715130891);
+            slashCommandsConfig.RegisterCommands<RollCommands>(GuildID); //Insert Server ID aka GuildID Here
 
             await Client.ConnectAsync();
             await Task.Delay(-1);
